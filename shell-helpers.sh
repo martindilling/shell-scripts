60941d42e294a47c4c966653038e82a213d7265e
Martin Dilling-Hansen <martindilling@gmail.com>
2016-01-04 22:13:30 +0000

    Shell helper in root and scripts in /bin

---
+++ shell-helpers.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+source "$DIR/helpers/variables.sh"
+source "$DIR/helpers/colours.sh"
+source "$DIR/helpers/alerts.sh"
+source "$DIR/helpers/if_system.sh"
+source "$DIR/helpers/if_flags.sh"
+source "$DIR/helpers/if_file.sh"
+source "$DIR/helpers/if_type.sh"
+source "$DIR/helpers/shell_script_functions.sh"
+source "$DIR/helpers/system_functions.sh"
+source "$DIR/helpers/ask_functions.sh"
+source "$DIR/helpers/file_functions.sh"
+source "$DIR/helpers/backup_functions.sh"
